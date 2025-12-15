ebfb483b38938ff75fbd800966b52ffc8b5d679e
MyeongjuHong <hmjpearl@gmail.com>
2025-12-15 14:25:42 +0900

    feat: 유저 기능 구현
    - 아직 연동 테스트 중이라 npm run dev시 에러가 납니다
    - 최대한 빨리 고쳐서 다시 PR하겠습니다

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,19 @@ import express from "express";
 import { setupSwagger } from "./docs/swagger";
 import dotenv from "dotenv";
 import "./services/discordBot";
+import authRouter from "./modules/auth/auth.routes";
 
 dotenv.config(); // .env 변수를 process.env 객체에 추가
 
 const app = express();
 app.use(express.json());
+// app.use(cookieParser()); // 왜 기본 세팅할 때 이거 세팅 안한거지..??
+
+// 모든 도메인 허용
+// app.use(cors()); // 엥 이것두..?
+
+// 라우트
+app.use("/auth", authRouter);
 
 setupSwagger(app);
 
